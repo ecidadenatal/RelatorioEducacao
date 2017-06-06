70e22585845e5303a76af069e2e0e555d1d8bd08
Igor Bezerra <igor.bezerra96@gmail.com>
2017-06-06 16:14:31 -0300

    Alterações no relatório de aluno votante

--- estrutura/instalacao.sql
+++ estrutura/instalacao.sql
@@ -10,3 +10,15 @@ create table plugins.transferencialoteobservacao (
   transferencialote integer not null,
   observacao text
 );
+
+CREATE SEQUENCE plugins.titulorelatorioalunoeleicao_sequencial_seq
+INCREMENT 1
+MINVALUE 1
+MAXVALUE 9223372036854775807
+START 1
+CACHE 1;
+
+create table plugins.titulorelatorioalunoeleicao (
+  sequencial integer not null primary key,
+  titulo text
+);
